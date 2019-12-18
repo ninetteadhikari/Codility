6ac36a4c1f1a8275e093001a0a329c9791be4ded
Ninette Adhikari <ninette_adhikari@hotmail.com>
2019-12-18 16:28:17 +0000

    Lesson1 BinaryGap

--- lessons/BinaryGap.js
+++ lessons/BinaryGap.js
@@ -1,23 +1,27 @@
-function getGap(binary){
-
-}
+// Lesson 1 Iterations | Binary Gap
 
 function binaryGap(N){
 let binary=N.toString(2)
+console.log("number: "+binary)
+
 let gap=0
+let maxGap=0
+let lastOne=binary.lastIndexOf(1)
 
-
-
-for (let i=0;i<binary.length;i++){
-  if(binary.charAt(i)===1 && binary.charAt(i+1)!==1){
-    gap=7
+for (let i=lastOne-1;i>=0;i--){
+if (binary.charAt(i)==="0"){
+  gap+=1
+}
+else {
+  if (maxGap < gap){
+    maxGap=gap
   }
+    gap=0
+}
+}
+return maxGap
 }
 
-if(binary.indexOf(1)!==binary.indexOf(1,binary.indexOf(1)+1)){
-  return binary.indexOf(1,binary.indexOf(1)+1)-binary.indexOf(1)-1
-}
-return gap
-}
-
-binaryGap(9)
+// binaryGap(9)
+// binaryGap(32)
+// binaryGap(1041)
