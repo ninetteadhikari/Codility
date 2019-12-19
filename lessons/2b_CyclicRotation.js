356dbd83b0bacd8fe3eb82e5817b2c1f424b31bf
Ninette Adhikari <ninette.adhikari@gmail.com>
2019-12-19 14:25:21 +0000

    Accounting for empty array

--- lessons/2b_CyclicRotation.js
+++ lessons/2b_CyclicRotation.js
@@ -1,6 +1,7 @@
 // Lesson 2b CyclicRotation
 
 function CyclicRotation(A,K){
+if(A.length!==0){
 if(K<A.length){
   A.unshift(...A.splice(-K))
 }
@@ -9,9 +10,11 @@ for (let i=1;i<=K;i++){
    A.unshift(A.pop())
 }
 }
+}
 return A
 }
 
 //  CyclicRotation([3, 8, 9, 7, 6],3)
 //  CyclicRotation([0,0,0],1)
 //  CyclicRotation([1,2,3,4],4)
+//  CyclicRotation([],4)
