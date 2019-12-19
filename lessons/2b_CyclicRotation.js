d12c6878c87cda85d4ad8fca948e2c71d39130c0
Ninette Adhikari <ninette.adhikari@gmail.com>
2019-12-19 14:21:05 +0000

    CyclicRotation

---
+++ lessons/2b_CyclicRotation.js
@@ -0,0 +1,17 @@
+// Lesson 2b CyclicRotation
+
+function CyclicRotation(A,K){
+if(K<A.length){
+  A.unshift(...A.splice(-K))
+}
+else{
+for (let i=1;i<=K;i++){
+   A.unshift(A.pop())
+}
+}
+return A
+}
+
+//  CyclicRotation([3, 8, 9, 7, 6],3)
+//  CyclicRotation([0,0,0],1)
+//  CyclicRotation([1,2,3,4],4)
