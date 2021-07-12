60badf59f81f485d584fcecbdb44219a62eb3b59
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-07-12 17:46:32 +0600

    fix: Add comments for MissingInteger

--- lessons/4c_MissingInteger.js
+++ lessons/4c_MissingInteger.js
@@ -1,19 +1,21 @@
+// Create counter, if counter is 0/missing or null return index/missing value.
+// This will capture negatives as they will be null
 function MissingInteger (A) {
   const counter = new Array(A.length + 1).fill(0)
-  console.log(counter);
   for (const value of A) {
-    console.log(value)
     counter[value] += 1
   }
-  console.log(counter)
   for (let i = 1; i <= counter.length; i++) {
     if (counter[i] === 0 || !counter[i]) {
-      console.log(i)
       return i
     }
   }
 }
 
+// Create counter with max value length. For negative max return 1, and for
+// positive check if there is a missing value in the counter. For the missing
+// value return the index of the missing space or if no missing then return
+// value after the max.
 function MissingInteger1 (A) {
   const max = Math.max(...A)
   let min
